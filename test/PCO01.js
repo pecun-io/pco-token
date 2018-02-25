32afc7edbea8b555ed74038c00b5814dadd87631
Gerhard Liebmann <office@pecun.io>
2018-02-25 21:04:36 +0100

    Added tests for token ownership, sending tokens to other address

--- test/PCO01.js
+++ test/PCO01.js
@@ -10,17 +10,26 @@ const web3 = new Web3(provider);
 const { interface, bytecode } = Compiler.compile('PCO01.sol', 'PCO01');
  
 
-let accounts;
-let inbox;
+let accounts,
+    inbox;
+
+let accountOwner, 
+    accountUser1,
+    accountUser2;
+
+const expTotalSupply = 10000000000000000;
  
 beforeEach(async () => {
     // Get a list of all accounts
     accounts = await web3.eth.getAccounts();
-
+    accountOwner = accounts[0];
+    accountUser1 = accounts[1];
+    accountUser2 = accounts[2];
+    
     // Use one of those accounts to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode, arguments: [] })
-        .send({ from: accounts[0], gas: '1000000' });
+        .send({ from: accountOwner, gas: '1000000' });
     
     inbox.setProvider(provider);
 });
@@ -34,15 +43,37 @@ describe('PCO01 - Tests', () => {
     it('has a default supply of 10^16 separate units', async () => {
         const message = await inbox.methods.totalSupply().call();
         const expTotalSupply = 10000000000000000;
-        console.log(message);
-        console.log(expTotalSupply);
         assert.equal(parseInt(message), expTotalSupply);
     });
-
+    
     it('has a default decimals of 8', async () => {
         const message = await inbox.methods.DECIMALS().call();
         assert.equal(message, 8);
     });
 
-    // Further contracts missing
+    it('contract owner owns all tokens', async () => {
+        const balance = await inbox.methods.balanceOf(accountOwner).call();
+        assert.equal(parseInt(balance), expTotalSupply);
+    });
+
+    it('send 10 tokens to user1 from contract owner', async () => {
+        let sendAmount = 10;
+        await inbox.methods.transfer(accountUser1, sendAmount).send({ from: accountOwner });
+        const balanceUser1 = await inbox.methods.balanceOf(accountUser1).call();
+        const balanceOwner = await inbox.methods.balanceOf(accountOwner).call();
+        
+        assert.equal(parseInt(balanceUser1), sendAmount);
+        assert.equal(parseInt(balanceOwner), expTotalSupply-sendAmount);
+    });
+
+/*    it('try overspend 20 tokens from user1 to user2', async () => {
+        let sendAmount = 10;
+        await inbox.methods.transfer(accountUser1, sendAmount).send({ from: accountOwner });
+        const balanceUser1 = await inbox.methods.balanceOf(accountUser1).call();
+        const balanceOwner = await inbox.methods.balanceOf(accountOwner).call();
+        
+        assert.equal(parseInt(balanceUser1), sendAmount);
+        assert.equal(parseInt(balanceOwner), expTotalSupply-sendAmount);
+    });
+*/
 });
